6daeb1c26a7b5a9e5085e8cb6de98a3c9e6b56bd
duhieu_b <benjamin.duhieu@epitech.eu>
2016-04-27 23:29:02 +0000

    Begin A*

--- include/astar.h
+++ include/astar.h
@@ -5,7 +5,7 @@
 ** Login   <brout_m@epitech.net>
 **
 ** Started on  Wed Apr 27 18:40:02 2016 marc brout
-** Last update Wed Apr 27 22:16:32 2016 benjamin duhieu
+** Last update Wed Apr 27 22:29:40 2016 benjamin duhieu
 */
 
 #ifndef ASTAR_H_
@@ -14,6 +14,8 @@
 # include "pars.h"
 
 
-void			calc_dist(t_case *last, t_case *origin, t_case *current);
+void			calc_dist(t_case *last,
+				  t_case *origin,
+				  t_case *current);
 
 #endif /* !ASTAR_H_ */
