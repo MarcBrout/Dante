2deefef43662b7e649fd85145ea9ad6d1e77205f
Marc <brout.marc@gmail.com>
2016-04-27 22:11:52 +0000

    clean

--- include/astar.h
+++ include/astar.h
@@ -5,12 +5,15 @@
 ** Login   <brout_m@epitech.net>
 **
 ** Started on  Wed Apr 27 18:40:02 2016 marc brout
-** Last update Wed Apr 27 18:41:53 2016 marc brout
+** Last update Wed Apr 27 22:11:45 2016 marc brout
 */
 
 #ifndef ASTAR_H_
 # define ASTAR_H_
 
 # include "pars.h"
+
+
+void			calc_dist(t_cas *last, t_cas *origin, t_cas *current);
 
 #endif /* !ASTAR_H_ */
